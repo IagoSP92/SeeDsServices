1d5c4dc86cd6c4852846d3fce9a9cdffcbfc4242
Alumno <Alumno@PC-INFOR00>
2019-01-28 10:45:12 +0100

    modificadas package declaration dao

--- src/com/isp/seeds/dao/spi/EtiquetaDAO.java
+++ src/com/isp/seeds/dao/spi/EtiquetaDAO.java
@@ -1,4 +1,4 @@
-package com.isp.seeds.dao.utils;
+package com.isp.seeds.dao.spi;
 
 import java.util.List;
 
--- src/com/isp/seeds/dao/spi/VideoDAO.java
+++ src/com/isp/seeds/dao/spi/VideoDAO.java
@@ -1,4 +1,4 @@
-package com.isp.seeds.dao.utils;
+package com.isp.seeds.dao.spi;
 
 import java.sql.Connection;
 import java.util.List;
